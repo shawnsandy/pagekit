5fcacc85e8ed863bf5ec6b62f9d73737027570cb
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-08-03 19:42:55 -0400

    clean up

--- src/PageKitServiceProvider.php
+++ src/PageKitServiceProvider.php
@@ -4,38 +4,45 @@ namespace ShawnSandy\PageKit;
 
 use Illuminate\Support\ServiceProvider;
 
+
 class PageKitServiceProvider extends ServiceProvider
 {
+
     /**
-     * Perform post-registration booting of services.
-     */
+    * Perform post-registration booting of services.
+    *
+    * @return null
+    */
     public function boot()
     {
         if (!$this->app->routesAreCached()) {
-            require __DIR__.'/routes.php';
+            require __DIR__ . '/routes.php';
         }
 
-        $this->loadViewsFrom(__DIR__.'/resources/views/pagekit', 'page');
+        $this->loadViewsFrom(__DIR__ . '/resources/views/pagekit', 'page');
 
-        $this->publishes([
-          __DIR__.'/resources/views/pagekit' => resource_path('views/vendor/page'),
-          __DIR__.'/resources/assets' => resource_path('assets/sass/pagekit'),
+        $this->publishes(
+            [ __DIR__.'/resources/views/pagekit'=>resource_path('views/vendor/page'),
+            __DIR__ . '/resources/assets'=>resource_path('assets/sass/pagekit')],
+            'views'
+        );
 
-      ], 'views');
+        $this->publishes(
+            [__DIR__.'/public/css/pagekit' => public_path('css/pagekit')], 'public'
+        );
 
-        $this->publishes([
-          __DIR__.'/public/css/pagekit' => public_path('css/pagekit'),
-      ], 'public');
+        $this->publishes(
+            [ __DIR__ . '/config/pagekit.php' => config_path('pagekit.php')],
+            'config'
+        );
 
-      $this->publishes([
-          __DIR__.'/config/pagekit.php' => config_path('pagekit.php')
-      ], 'config');
-      
     }
 
     /**
-     * Register any package services.
-     */
+    * Register any package services.
+    *
+    * @return null
+    */
     public function register()
     {
         //
