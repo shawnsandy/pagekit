af68190a7ca5131e6d0670f1acffe8e18e062fd9
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-08-04 17:07:39 -0400

    Missing config id FIX
    Adds missing config param id (pagekit) to branding config

--- src/resources/views/pagekit/page-layouts/default.blade.php
+++ src/resources/views/pagekit/page-layouts/default.blade.php
@@ -7,25 +7,25 @@
     <link rel="stylesheet" href="/css/pagekit/app.css">
      <link rel="stylesheet" href="/css/pagekit/page.css">
 
-     @if(config('config.branding'))
+     @if(config('pagekit.branding'))
     <styles>
     body {
-        background-color: "{{ config('brand.background-color')}}";
-        color: "{{ config('brand.text-color')}}";
-        font-family: "{{ config('brand.font-family' )}}";
+        background-color: "{{ config('pagekit.brand.background-color')}}";
+        color: "{{ config('pagekit.brand.text-color')}}";
+        font-family: "{{ config('pagekit.brand.font-family' )}}";
     }
     header {
-        background-color: "{{ config('brand.header-background-color', none )}}";
-        background-image: "{{ config('brand.header-backgroung-image') }}";
+        background-color: "{{ config('pagekit.brand.header-background-color', none )}}";
+        background-image: "{{ config('pagekit.brand.header-backgroung-image') }}";
     }
 
     header h1 {
-        color: "{{ config('brand.header-font-color') }}";
-        font-size: "{{ config('brand.font-size')}}";
+        color: "{{ config('pagekit.brand.header-font-color') }}";
+        font-size: "{{ config('pagekit.brand.font-size')}}";
     }
     footer {
-        background-color: "{{ config('brand.footer-background-color')}}";
-        color: "{{ config('brand.footer-color')}}";
+        background-color: "{{ config('pagekit.brand.footer-background-color')}}";
+        color: "{{ config('pagekit.brand.footer-color')}}";
     }
     </styles>
     @endif
