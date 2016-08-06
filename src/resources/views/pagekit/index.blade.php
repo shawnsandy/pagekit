4af170af9b92bdd17ff53b701a95345568b98d6e
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-08-06 00:26:16 -0400

    Header uses shared header

--- src/resources/views/pagekit/index.blade.php
+++ src/resources/views/pagekit/index.blade.php
@@ -2,23 +2,8 @@
 
 @section('page')
 
-<header class="landing" data-aos="fade-up">
-    <section class="cover">
-        <div class="layout-table">
-            <div class="layout-row">
-                <div class="logo">
-                    <h1>{{ config('pagekit.company_name', 'Laravel.PageKit' ) }}</h1>
-                    <p class="lead">
-                    @if(config('pagekit.hide_header_title'))
-                        {{ config('pagekit.title_slug', 'Quick start pages for Laravel.' )}}
-                    @endif
-                    </p>
-                </div>
-            </div>
-        </div>
-    </section>
+@include('page::shared.header', ["header_class" => "landing"])
 
-</header>
 
 @include('page::shared.nav')
 
