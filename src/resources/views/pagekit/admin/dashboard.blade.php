9fb2f4e14c6d0e0eb7b2d88f7822c61cc9e25531
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-08-11 07:47:22 -0400

    Basic setup and layout for page/admin/view

--- src/resources/views/pagekit/admin/dashboard.blade.php
+++ src/resources/views/pagekit/admin/dashboard.blade.php
@@ -2,12 +2,19 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-2">
-                <h3 class="lead">Administration</h3>
+            <div class="col-md-12">
+                <h1 class="">Dashboard</h1>
+                <hr>
             </div>
-            <div class="col-md-10">
-                <h3>Dashboard</h3>
-            </div>
+            <section>
+                   <div class="col-md-8">
+                       <h2>Section Title</h2>
+                   </div>
+                   <div class="col-md-4">
+                       <h3>Sidebar</h3>
+                   </div>
+            </section>
         </div>
+
     </div>
 @endsection
--- src/resources/views/pagekit/page-layouts/default.blade.php
+++ src/resources/views/pagekit/page-layouts/default.blade.php
@@ -2,18 +2,30 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>{{ config('pagekit.title', 'Page Title - PageKit.') }} </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <title>{{ config('pagekit.title', 'Page Title - PageKit.') }} </title>
     <link rel="stylesheet" href="/css/pagekit/app.css">
-     <link rel="stylesheet" href="/css/pagekit/page.css">
+    <link rel="stylesheet" href="/css/pagekit/page.css">
 
      @if(config('pagekit.branding'))
     <style>
+
       body {
         background-color: {!! config('pagekit.brand.background_color') !!};
         color: {!! config('pagekit.brand.text_color')!!};
         font-family: {!! config('pagekit.brand.font_family' ) !!};
     }
+
     header {
         background-color: {!! config('pagekit.brand.header_background_color') !!};
         color: {{ config('pagekit.brand.header_font_color')}};
@@ -31,10 +43,12 @@
         color: {!! config('pagekit.brand.header_font_color') !!};
         font-size: {!! config('pagekit.brand.font_size') !!};
     }
+
     footer {
         background-color: {!! config('pagekit.brand.footer_background_color') !!};
         color: {!! config('pagekit.brand.footer_color') !!};
     }
+
     </style>
     @endif
 
