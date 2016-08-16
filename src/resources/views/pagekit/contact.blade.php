5f18fab849e6b0d534b27c647ea136c73b491162
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-08-15 22:32:16 -0400

    [WIP] Contact page

--- src/resources/views/pagekit/contact.blade.php
+++ src/resources/views/pagekit/contact.blade.php
@@ -1,51 +1,40 @@
 @extends('page::page-layouts.main-page')
 @section('content')
+
     <div class="container">
         <div class="row" data-aos="zomm-in">
-
-            <div class="col-md-6">
-                <h3>Contact Us</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloribus explicabo, id ipsa iure
-                    optio pariatur
-                    praesentium qui quos similique sit totam voluptate, voluptatum! Accusantium dignissimos dolore omnis
-                    quae quasi,
-                    suscipit velit voluptates. Quisquam, reprehenderit?</p>
-
-                <p>Aut culpa cum, debitis molestias neque voluptates!
-                    Iusto, magni, odio? Assumenda ducimus explicabo id inventore natus nihil nostrum officiis quos.
-                    Assumenda
-                    excepturi impedit minus mollitia nam, qui, quo rem reprehenderit repudiandae similique soluta sunt,
-                    voluptatibus?</p>
-            </div>
-            <div class="col-md-6">
-                <h3>Email</h3>
-                <form id="contact-form" action="/">
-                    {{ csrf_field() }}
-                    <p>
-                        <label for="full_name">Your Name</label>
-                        <input type="text" name="full_name" class="form-control" placeholder="Enter your name">
+            <h1 class="text-center headline"><i class="fa fa-envelope" aria-hidden="true"></i></h1>
+            <section>
+                <h1 class="text-capitalize text-center">We would love to hear from you.</h1>
+                <p class="text-center lead">
+                    Conveniently transform one-to-one markets without cross-media leadership. Continually optimize
+                    visionary outsourcing with end-to-end vortals. Phosfluorescently matrix efficient materials.
+                </p>
+            </section>
+            <section id="social">
+                <div class="container">
+                    <p class="lead text-center">
+                        VIA SOCIAL MEDIA
                     </p>
-                    <p>
-                        <label for="email">Email</label>
-                        <input type="email" name="email" class="form-control" placeholder="Email address">
-                    </p>
-                    <p>
-                        <label for="subject">Subject</label>
-                        <input type="text" name="subject" class="form-control" placeholder="Subject">
-                    </p>
-                    <p>
-                        <label for="subject">Message</label>
-                        <textarea name="message" id="subject" cols="30" rows="10" class="form-control"></textarea>
-                    </p>
-                    <p>
-                        <button class="btn btn-success" type="submit">Submit</button>
-                    </p>
+                </div>
+            </section>
+            <section>
 
-                </form>
+                <div class="row">
+                   <h1 class="text-center headline"><i class="fa fa-envelope" aria-hidden="true"></i></h1>
+                    <div class="col-md-6">
+                        @include('page::shared.contact-form')
+                    </div>
 
-            </div>
+                    <div class="col-md-6">
+                        @include('page::shared.location-map')
 
+                    </div>
+                </div>
+
+            </section>
 
         </div>
     </div>
+
 @endsection
---
+++ src/resources/views/pagekit/shared/contact-form.blade.php
@@ -0,0 +1,25 @@
+
+<form id="contact-form" action="/">
+    {{ csrf_field() }}
+    <p>
+        {{--<label for="full_name">Your Name</label>--}}
+        <input type="text" name="full_name" class="form-control" placeholder="Enter your name">
+    </p>
+    <p>
+        {{--<label for="email">Email</label>--}}
+        <input type="email" name="email" class="form-control" placeholder="Email address">
+    </p>
+    <p>
+        {{--<label for="subject">Subject</label>--}}
+        <input type="text" name="subject" class="form-control" placeholder="Subject">
+    </p>
+    <p>
+        {{--<label for="subject">Message</label>--}}
+        <textarea name="message" id="subject" cols="30" rows="8" class="form-control"></textarea>
+    </p>
+    <p>
+        <button class="btn btn-success" type="submit">Submit</button>
+    </p>
+
+</form>
+
