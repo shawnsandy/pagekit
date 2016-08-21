a9404cdc9870597840c747fe1973db60ea4abc25
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-08-21 09:56:49 -0400

    login views

--- src/resources/views/pagekit/login.blade.php
+++ src/resources/views/pagekit/login.blade.php
@@ -1,2 +1,11 @@
-<?php
+@extends('page::page-layouts.main-page')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                @include('page::shared.login-form')
+            </div>
+        </div>
+    </div>
 
+@endsection
