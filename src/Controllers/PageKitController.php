27a539e15a9245b3e3bbdccd13b65eefef6f7093
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-09-05 08:42:41 -0400

    Fixes type hint issue on mail closure

--- src/Controllers/PageKitController.php
+++ src/Controllers/PageKitController.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Mail\Message;
 use Mail;
 
 class PageKitController extends Controller
@@ -15,9 +16,9 @@ class PageKitController extends Controller
      */
     public function contactUs(Request $request)
     {
-        Mail::send('page::emails.contact-info', ['data' => $request->all()], function ($m) use ($request) {
-            $m->from($request->email, ': Contact request');
-            $m->to('shawnasndy04@gmail.com', 'shawn sandy')->subject('Your Reminder!');
+        Mail::send('page::emails.contact-info', ['data' => $request->all()], function (Message $message) use ($request) {
+            $message->from($request->email, ': Contact request');
+            $message->to('shawnasndy04@gmail.com', 'shawn sandy')->subject('Your Reminder!');
         });
 
         return back();
